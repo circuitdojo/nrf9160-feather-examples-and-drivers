e7d982ccfe69b05c7d830c72686d79bc99900e1c
Jared Wolff <hello@jaredwolff.com>
2020-11-02 18:28:00 -0500

    Adding external RTC example.

---
+++ samples/external_rtc/src/main.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2020 Circuit Dojo LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/gpio.h>
+#include <sys/util.h>
+#include <sys/printk.h>
+#include <inttypes.h>
+#include <drivers/counter.h>
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+/* RTC control */
+struct device *rtc;
+
+static void rtc_init()
+{
+
+	/* Get the device */
+	rtc = device_get_binding("PCF85063A");
+	if (rtc == NULL)
+	{
+		LOG_ERR("Failed to get RTC device binding");
+		return;
+	}
+
+	LOG_INF("device is %p, name is %s", rtc, log_strdup(rtc->name));
+
+	/* 2 seconds */
+	const struct counter_alarm_cfg cfg = {
+			.ticks = 10,
+	};
+
+	/* Set the alarm */
+	int ret = counter_set_channel_alarm(rtc, 0, &cfg);
+	if (ret)
+	{
+		LOG_ERR("Unable to set alarm");
+	}
+}
+
+static bool timer_flag = false;
+
+void main(void)
+{
+
+	/* Init RTC */
+	rtc_init();
+
+	while (true)
+	{
+		if (!timer_flag)
+		{
+			int ret = counter_get_pending_int(rtc);
+
+			if (ret == 1)
+			{
+				LOG_INF("Timer event!");
+
+				timer_flag = true;
+
+				int ret = counter_cancel_channel_alarm(rtc, 0);
+				if (ret)
+				{
+					LOG_ERR("Unable to cancel channel alarm!");
+				}
+			}
+		}
+
+		k_sleep(K_MSEC(100));
+	}
+}
