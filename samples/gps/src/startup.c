3d3d581bc409245808208b7d87ce6fbe5c1af207
Jared Wolff <hello@jaredwolff.com>
2021-09-11 12:09:07 -0400

    Power mode for GPS sample (nRF9160 Feather v4)
    Added:
    * Power control (PWM vs PFM) for latest version of board

---
+++ samples/gps/src/startup.c
@@ -0,0 +1,23 @@
+
+#include <zephyr.h>
+#include <drivers/gpio.h>
+#include <logging/log.h>
+
+#define POWER_MODE_PIN 13
+
+const static struct device *gpio;
+
+static int gps_sample_setup(const struct device *arg)
+{
+
+	/* Gpio pin */
+	gpio = device_get_binding(DT_LABEL(DT_NODELABEL(gpio0)));
+	__ASSERT(gpio, "Failed to get the gpio0 device");
+
+    /* Set low */
+    gpio_pin_configure(gpio, POWER_MODE_PIN, GPIO_OUTPUT_LOW);
+
+    return 0;
+}
+
+SYS_INIT(gps_sample_setup, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
