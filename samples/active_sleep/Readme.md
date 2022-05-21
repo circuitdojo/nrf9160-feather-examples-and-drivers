f96e8f82eedb17563ef96850af623554b6b15d0d
Jared Wolff <hello@jaredwolff.com>
2022-05-21 09:39:27 -0400

    Active sleep update
    Changed:
    * Improving sleep current. Currently around 30uA

---
+++ samples/active_sleep/Readme.md
@@ -0,0 +1,10 @@
+# Active Sleep Sample
+
+A quick note on this sample.
+
+This sample connects to LTE and goes into PSM mode. Once connected both the processor and the modem firmware should go into a deep sleep.
+
+As of 5/21/22 the measurements on a nRF9160 Feather V4 are averaging:
+
+* 30uA at a 3.3V battery
+* 28uA at a 3.6V battery
--- samples/active_sleep/src/main.c
+++ samples/active_sleep/src/main.c
@@ -7,13 +7,45 @@
 #include <zephyr.h>
 #include <modem/lte_lc.h>
 #include <devicetree.h>
+#include <drivers/sensor.h>
+
+static void setup_accel(void)
+{
+	const struct device *sensor = DEVICE_DT_GET(DT_INST(0, st_lis2dh));
+
+	if (sensor == NULL || !device_is_ready(sensor))
+	{
+		printk("Could not get %s device\n",
+			   DT_LABEL(DT_INST(0, st_lis2dh)));
+		return;
+	}
+
+	// Disable the device
+	struct sensor_value odr = {
+		.val1 = 0,
+	};
+
+	int rc = sensor_attr_set(sensor, SENSOR_CHAN_ACCEL_XYZ,
+							 SENSOR_ATTR_SAMPLING_FREQUENCY,
+							 &odr);
+	if (rc != 0)
+	{
+		printk("Failed to set odr: %d\n", rc);
+		return;
+	}
+}
 
 void main(void)
 {
 
+	/* Disable accel */
+	setup_accel();
+
+	/* Power saving */
+	lte_lc_psm_req(true);
+
 	/* Init modem library */
-	lte_lc_init();
-	lte_lc_power_off();
+	lte_lc_init_and_connect();
 
 	while (1)
 	{
