c1706cec4cf26b47078902d5ab424b1dc2fc5bdf
Jared Wolff <hello@jaredwolff.com>
2023-08-16 11:35:28 -0400

    Converting to ZCBOR

--- samples/tracker/src/codec/app_codec.c
+++ samples/tracker/src/codec/app_codec.c
@@ -6,7 +6,8 @@
 
 #include <app_codec.h>
 
-#include <qcbor/qcbor_encode.h>
+#include <zcbor_decode.h>
+#include <zcbor_encode.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app_codec);
@@ -15,112 +16,169 @@ int app_codec_motion_encode(struct app_motion_data *p_payload, uint8_t *p_buf, s
 {
 
     // Setup of the goods
-    UsefulBuf buf = {
-        .ptr = p_buf,
-        .len = buf_len};
-    QCBOREncodeContext ec;
-    QCBOREncode_Init(&ec, buf);
+    ZCBOR_STATE_E(es, 0, p_buf, buf_len, 0);
 
     /* Create over-arching map */
-    QCBOREncode_OpenMap(&ec);
+    bool ok = zcbor_map_start_encode(es, 4);
+    if (!ok)
+    {
+        LOG_ERR("Did not start CBOR map correctly. Err: %i", zcbor_peek_error(es));
+        return -ENOMEM;
+    }
 
     /* Add coordinates */
-    QCBOREncode_AddDoubleToMap(&ec, "x", sensor_value_to_double(&p_payload->x));
-    QCBOREncode_AddDoubleToMap(&ec, "y", sensor_value_to_double(&p_payload->y));
-    QCBOREncode_AddDoubleToMap(&ec, "z", sensor_value_to_double(&p_payload->z));
+    zcbor_tstr_put_lit(es, "x");
+    zcbor_float64_put(es, sensor_value_to_double(&p_payload->x));
+
+    zcbor_tstr_put_lit(es, "y");
+    zcbor_float64_put(es, sensor_value_to_double(&p_payload->y));
+
+    zcbor_tstr_put_lit(es, "z");
+    zcbor_float64_put(es, sensor_value_to_double(&p_payload->z));
 
     /* Timestamp */
     if (p_payload->ts > 0)
-        QCBOREncode_AddUInt64ToMap(&ec, "ts", p_payload->ts);
+    {
+        zcbor_tstr_put_lit(es, "ts");
+        zcbor_uint64_put(es, p_payload->ts);
+    }
 
     /* Close map */
-    QCBOREncode_CloseMap(&ec);
+    ok = zcbor_map_end_encode(es, 4);
+    if (!ok)
+    {
+        LOG_ERR("Did not encode CBOR map correctly. Err: %i", zcbor_peek_error(es));
+        return -ENOMEM;
+    }
+
+    *p_size = es->payload - p_buf;
+    LOG_INF("Size: %i", *p_size);
 
     /* Finish things up */
-    return QCBOREncode_FinishGetSize(&ec, p_size);
+    return 0;
 }
 
 int app_codec_gps_encode(struct app_gps_data *p_payload, uint8_t *p_buf, size_t buf_len, size_t *p_size)
 {
-
     // Setup of the goods
-    UsefulBuf buf = {
-        .ptr = p_buf,
-        .len = buf_len};
-    QCBOREncodeContext ec;
-    QCBOREncode_Init(&ec, buf);
+    ZCBOR_STATE_E(es, 0, p_buf, buf_len, 0);
 
     /* Create over-arching map */
-    QCBOREncode_OpenMap(&ec);
+    bool ok = zcbor_map_start_encode(es, 4); // Assuming 4 key-value pairs like the previous example, adjust accordingly
+    if (!ok)
+    {
+        LOG_ERR("Did not start CBOR map correctly. Err: %i", zcbor_peek_error(es));
+        return -ENOMEM;
+    }
 
     /* Add coordinates */
-    QCBOREncode_AddDoubleToMap(&ec, "lat", p_payload->data.latitude);
-    QCBOREncode_AddDoubleToMap(&ec, "lng", p_payload->data.longitude);
+    zcbor_tstr_put_lit(es, "lat");
+    zcbor_float64_put(es, p_payload->data.latitude);
+
+    zcbor_tstr_put_lit(es, "lng");
+    zcbor_float64_put(es, p_payload->data.longitude);
 
     /* Add remaining parameters */
-    QCBOREncode_AddDoubleToMap(&ec, "alt", p_payload->data.altitude);
+    zcbor_tstr_put_lit(es, "alt");
+    zcbor_float64_put(es, p_payload->data.altitude);
 
     /* Timestamp */
     if (p_payload->ts > 0)
-        QCBOREncode_AddUInt64ToMap(&ec, "ts", p_payload->ts);
+    {
+        zcbor_tstr_put_lit(es, "ts");
+        zcbor_uint64_put(es, p_payload->ts);
+    }
 
     /* Close map */
-    QCBOREncode_CloseMap(&ec);
+    ok = zcbor_map_end_encode(es, 4);
+    if (!ok)
+    {
+        LOG_ERR("Did not encode CBOR map correctly. Err: %i", zcbor_peek_error(es));
+        return -ENOMEM;
+    }
+
+    *p_size = es->payload - p_buf; // Use a pointer to modify the value
+    LOG_INF("Size: %i", *p_size);
 
     /* Finish things up */
-    return QCBOREncode_FinishGetSize(&ec, p_size);
+    return 0;
 }
 
 int app_codec_device_info_encode(struct app_modem_info *p_payload, uint8_t *p_buf, size_t buf_len, size_t *p_size)
 {
-
     // Setup of the goods
-    UsefulBuf buf = {
-        .ptr = p_buf,
-        .len = buf_len};
-    QCBOREncodeContext ec;
-    QCBOREncode_Init(&ec, buf);
+    ZCBOR_STATE_E(es, 0, p_buf, buf_len, 0);
 
     /* Create over-arching map */
-    QCBOREncode_OpenMap(&ec);
+    bool ok = zcbor_map_start_encode(es, 6); // There are 6 top-level key-value pairs
+    if (!ok)
+    {
+        LOG_ERR("Did not start CBOR map correctly. Err: %i", zcbor_peek_error(es));
+        return -ENOMEM;
+    }
 
     /* Battery voltage */
-    QCBOREncode_AddUInt64ToMap(&ec, "vbat", p_payload->data.device.battery.value);
+    zcbor_tstr_put_lit(es, "vbat");
+    zcbor_uint64_put(es, p_payload->data.device.battery.value);
 
     /* Network stuff */
-    QCBOREncode_OpenMapInMap(&ec, "nw");
-    QCBOREncode_AddUInt64ToMap(&ec, "rsrp", p_payload->rsrp);
-    QCBOREncode_AddUInt64ToMap(&ec, "area", p_payload->data.network.area_code.value);
-    QCBOREncode_AddUInt64ToMap(&ec, "mnc", p_payload->data.network.mnc.value);
-    QCBOREncode_AddUInt64ToMap(&ec, "mcc", p_payload->data.network.mcc.value);
-    QCBOREncode_AddUInt64ToMap(&ec, "cell", p_payload->data.network.cellid_hex.value);
-    QCBOREncode_AddSZStringToMap(&ec, "ip", p_payload->data.network.ip_address.value_string);
-    QCBOREncode_AddUInt64ToMap(&ec, "band", p_payload->data.network.current_band.value);
-    QCBOREncode_AddUInt64ToMap(&ec, "m_gps", p_payload->data.network.gps_mode.value);
-    QCBOREncode_AddUInt64ToMap(&ec, "m_lte", p_payload->data.network.lte_mode.value);
-    QCBOREncode_AddUInt64ToMap(&ec, "m_nb", p_payload->data.network.nbiot_mode.value);
-    QCBOREncode_CloseMap(&ec);
+    zcbor_tstr_put_lit(es, "nw");
+    zcbor_map_start_encode(es, 11); // There are 11 key-value pairs in "nw" map
+    zcbor_tstr_put_lit(es, "rsrp");
+    zcbor_uint64_put(es, p_payload->rsrp);
+    zcbor_tstr_put_lit(es, "area");
+    zcbor_uint64_put(es, p_payload->data.network.area_code.value);
+    zcbor_tstr_put_lit(es, "mnc");
+    zcbor_uint64_put(es, p_payload->data.network.mnc.value);
+    zcbor_tstr_put_lit(es, "mcc");
+    zcbor_uint64_put(es, p_payload->data.network.mcc.value);
+    zcbor_tstr_put_lit(es, "cell");
+    zcbor_uint64_put(es, p_payload->data.network.cellid_hex.value);
+    zcbor_tstr_put_lit(es, "ip");
+    zcbor_tstr_put_term(es, p_payload->data.network.ip_address.value_string);
+    zcbor_tstr_put_lit(es, "band");
+    zcbor_uint64_put(es, p_payload->data.network.current_band.value);
+    zcbor_tstr_put_lit(es, "m_gps");
+    zcbor_uint64_put(es, p_payload->data.network.gps_mode.value);
+    zcbor_tstr_put_lit(es, "m_lte");
+    zcbor_uint64_put(es, p_payload->data.network.lte_mode.value);
+    zcbor_tstr_put_lit(es, "m_nb");
+    zcbor_uint64_put(es, p_payload->data.network.nbiot_mode.value);
+    zcbor_map_end_encode(es, 11);
 
     /* SIM Stuff*/
-    QCBOREncode_OpenMapInMap(&ec, "sim");
-    QCBOREncode_AddSZStringToMap(&ec, "iccid", p_payload->data.sim.iccid.value_string);
-    QCBOREncode_CloseMap(&ec);
-
-    // LOG_INF("sim");
+    zcbor_tstr_put_lit(es, "sim");
+    zcbor_map_start_encode(es, 1); // There is 1 key-value pair in "sim" map
+    zcbor_tstr_put_lit(es, "iccid");
+    zcbor_tstr_put_term(es, p_payload->data.sim.iccid.value_string);
+    zcbor_map_end_encode(es, 1);
 
     /* Versions/board info */
-    QCBOREncode_OpenMapInMap(&ec, "inf");
-    QCBOREncode_AddSZStringToMap(&ec, "modv", p_payload->data.device.modem_fw.value_string);
-    QCBOREncode_AddSZStringToMap(&ec, "brdv", p_payload->data.device.board);
-    QCBOREncode_AddSZStringToMap(&ec, "appv", p_payload->data.device.app_version);
-    QCBOREncode_CloseMap(&ec);
+    zcbor_tstr_put_lit(es, "inf");
+    zcbor_map_start_encode(es, 3); // There are 3 key-value pairs in "inf" map
+    zcbor_tstr_put_lit(es, "modv");
+    zcbor_tstr_put_term(es, p_payload->data.device.modem_fw.value_string);
+    zcbor_tstr_put_lit(es, "brdv");
+    zcbor_tstr_put_term(es, p_payload->data.device.board);
+    zcbor_tstr_put_lit(es, "appv");
+    zcbor_tstr_put_term(es, p_payload->data.device.app_version);
+    zcbor_map_end_encode(es, 3);
 
-    /* Add timetstamp */
-    QCBOREncode_AddUInt64ToMap(&ec, "ts", p_payload->ts);
+    /* Add timestamp */
+    zcbor_tstr_put_lit(es, "ts");
+    zcbor_uint64_put(es, p_payload->ts);
 
     /* Close map */
-    QCBOREncode_CloseMap(&ec);
+    ok = zcbor_map_end_encode(es, 6);
+    if (!ok)
+    {
+        LOG_ERR("Did not encode CBOR map correctly. Err: %i", zcbor_peek_error(es));
+        return -ENOMEM;
+    }
+
+    *p_size = es->payload - p_buf;
+    LOG_INF("Size: %i", *p_size);
 
     /* Finish things up */
-    return QCBOREncode_FinishGetSize(&ec, p_size);
+    return 0;
 }
