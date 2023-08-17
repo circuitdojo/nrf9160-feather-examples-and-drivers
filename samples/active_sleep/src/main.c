10ed8c35d0b2c90ead5bbb5eb03bdff7c297647a
Jared Wolff <hello@jaredwolff.com>
2023-08-16 21:52:41 -0400

    Active sleep changes for 2.4.x

--- samples/active_sleep/src/main.c
+++ samples/active_sleep/src/main.c
@@ -5,17 +5,14 @@
  */
 
 #include <zephyr/kernel.h>
-#include <modem/lte_lc.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/pm/device.h>
 
-/* Devices */
-static const struct device *uart_dev = DEVICE_DT_GET(DT_NODELABEL(uart0));
-static const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
-static const struct device *spi3_dev = DEVICE_DT_GET(DT_NODELABEL(spi3));
+#include <modem/lte_lc.h>
+#include <modem/nrf_modem_lib.h>
 
 /* Gpios */
 static const struct gpio_dt_spec sw0 = GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
@@ -50,19 +47,13 @@ static void setup_accel(void)
 static int setup_gpio(void)
 {
 
-	if (!device_is_ready(sw0.port))
-	{
-		printk("GPIO device not ready\n");
-		return -EIO;
-	}
-
 	gpio_pin_configure(sw0.port,
 					   sw0.pin,
 					   GPIO_DISCONNECTED);
 
 	gpio_pin_configure(led0.port,
 					   led0.pin,
-					   GPIO_OUTPUT_LOW);
+					   GPIO_DISCONNECTED);
 
 	gpio_pin_configure(latch_en.port,
 					   latch_en.pin,
@@ -73,62 +64,15 @@ static int setup_gpio(void)
 
 int setup_uart()
 {
-	int err;
 
-	/* Initialize the UART module */
-	if (!device_is_ready(uart_dev))
+	static const struct device *const console_dev =
+		DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+	/* Disable console UART */
+	int err = pm_device_action_run(console_dev, PM_DEVICE_ACTION_SUSPEND);
+	if (err < 0)
 	{
-		printk("Cannot bind UART device\n");
-		return -EIO;
-	}
-
-	/* Power off UART module */
-	uart_rx_disable(uart_dev);
-	k_sleep(K_MSEC(100));
-	err = pm_device_action_run(uart_dev, PM_DEVICE_ACTION_SUSPEND);
-	if (err)
-	{
-		printk("Can't power off uart: %d\n", err);
-		return err;
-	}
-
-	return 0;
-}
-
-int setup_i2c()
-{
-	int err;
-
-	if (!device_is_ready(i2c_dev))
-	{
-		printk("Cannot bind I2C device\n");
-		return -EIO;
-	}
-
-	err = pm_device_action_run(i2c_dev, PM_DEVICE_ACTION_SUSPEND);
-	if (err)
-	{
-		printk("Can't power off I2C: %d\n", err);
-		return err;
-	}
-
-	return 0;
-}
-
-int setup_spi()
-{
-	int err;
-
-	if (!device_is_ready(spi3_dev))
-	{
-		printk("Cannot bind SPI device\n");
-		return -EIO;
-	}
-
-	err = pm_device_action_run(spi3_dev, PM_DEVICE_ACTION_SUSPEND);
-	if (err)
-	{
-		printk("Can't power off spi: %d\n", err);
+		printk("Unable to suspend console UART. (err: %d)\n", err);
 		return err;
 	}
 
@@ -145,16 +89,13 @@ int main(void)
 	/* Disable accel */
 	setup_accel();
 
-	/* Power saving */
-	lte_lc_psm_req(true);
-	lte_lc_edrx_req(false);
-
-	/* Uart */
-	setup_uart();
-
-	/* Init modem library */
+	/* Init modem */
+	nrf_modem_lib_init();
 	lte_lc_init();
 	lte_lc_power_off();
+
+	/* Peripherals */
+	setup_uart();
 
 	return 0;
 }
