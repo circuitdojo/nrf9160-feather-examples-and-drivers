ea75c74e395d15af47a7b30a08b93c51fc0ab6f6
Marko Puric <marko@golioth.io>
2026-01-05 12:33:56 +0100

    sysbuild: allow app override of static partitions
    Only set circuitdojo_feather_nrf9151_pm_static.yml when
    CONFIG_CIRCUITDOJO_FEATHER_NRF9151_STATIC_PARTITIONS_FACTORY
    is enabled.
    
    Signed-off-by: Marko Puric <marko@golioth.io>

--- boards/circuitdojo/feather_nrf9151/sysbuild.cmake
+++ boards/circuitdojo/feather_nrf9151/sysbuild.cmake
@@ -4,5 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/circuitdojo_feather_nrf9151_pm_static.yml CACHE INTERNAL "")
+if (CONFIG_CIRCUITDOJO_FEATHER_NRF9151_STATIC_PARTITIONS_FACTORY)
+  set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/circuitdojo_feather_nrf9151_pm_static.yml CACHE INTERNAL "")
+endif()
 
