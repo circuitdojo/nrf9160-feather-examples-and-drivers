e424d2ce28eda79d0bdf0d5766fa0a707b5c8793
Jared Wolff <hello@jaredwolff.com>
2026-06-21 09:56:35 -0400

    boards: migrate Circuit Dojo nRF91 partitions to DTS

--- boards/circuitdojo/feather_nrf9160/sysbuild.cmake
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2024 Circuit Dojo LLC
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/circuitdojo_feather_nrf9160_pm_static.yml CACHE INTERNAL "")
-
--- boards/circuitdojo/feather_nrf9161/sysbuild.cmake
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2024 Circuit Dojo LLC
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/circuitdojo_feather_nrf9161_pm_static.yml CACHE INTERNAL "")
-
